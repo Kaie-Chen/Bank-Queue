a98811cc715f103c9a4b6353a0497377ec0c6fbe
kaie <kaiechen1001@gmail.com>
2022-10-07 13:29:50 -0700

    Hello

--- Bank.cpp
+++ Bank.cpp
@@ -11,6 +11,8 @@ struct Comparator{
 	}
 };
 
+
+
 void greedy(){
 	int numOfPeople = 0;
 	int timeTillClosure = 0;
@@ -30,20 +32,48 @@ void greedy(){
 
 //	cout << "\n \n";
 
+	/*
+10 10
+2000 0
+100 0
+3000 3
+200 5
+100 7
+200 7
+300 7
+400 7
+500 7
+500 7
+
+	*/
+
 	priority_queue<int> customerCash;
+	int customersDealtWith = 0;
 
 	for (int i = 0; i < timeTillClosure; i++){
 		customerCash = priority_queue<int>();
 
-//		cout << Clients.top().second << " ";
-		while (!Clients.empty() && Clients.top().first == i){
-//			cout << Clients.top().second << " \n";
-			customerCash.push(Clients.top().second);
-			Clients.pop();
-		}
 
-		if (!customerCash.empty()){
-			amountInBank += customerCash.top();
+//		cout << Clients.top().second << " ";
+		if (!Clients.empty()){
+			if (Clients.top().first == i){
+				while (!Clients.empty() && Clients.top().first == i){
+//					cout << "Client leaving right now at" << i << " Cash they have " << Clients.top().second <<  "\n";
+		//			cout << Clients.top().second << " \n";
+					customerCash.push(Clients.top().second);
+					Clients.pop();
+				}
+			} else if (Clients.top().first > i){
+//				cout << "No one leaving right now at time " << i << " dealing with next customer: " << Clients.top().first << " They have $" << Clients.top().second << "\n";
+				customerCash.push(Clients.top().second);
+				Clients.pop();
+			}
+
+
+			if (!customerCash.empty()){
+				amountInBank += customerCash.top();
+	//			cout << amountInBank << " --> ";
+			}
 		}
 
 	}
